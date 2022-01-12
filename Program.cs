55f526a7e6581d3a495ddf919db960c38e0c9136
Daniel Zamanillo <dzamanillok@gmail.com>
2022-01-12 14:29:24 -0600

    lesson 2 complete

--- Program.cs
+++ Program.cs
@@ -1,39 +1,45 @@
-﻿// // V6.0
-// Console.WriteLine("Hello, World!");
-
-
-//V5.?
-using System.Collections.Generic;
-
-
-namespace CatWorx.BadgeMaker
+﻿namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<Employee> GetEmployees()
         {
-            List<string> employees = new List<string>(){
-                "Adam",
-                "Alex",
-                "Bryan",
-                "Dan",
-                "Dom",
-                "JP",
-                "Wils"
-            };
-
-            employees.Add("Brooke");
-            employees.Add("Link");
-
-            foreach (string employee in employees)
+            List<Employee> employees = new();
+            while (true)
             {
-                Console.WriteLine(employee);
+                Console.WriteLine("Please enter a first name: (leave empty to exit): ");
+                string first = Console.ReadLine();
+                if (first == "")
+                {
+                    break;
+                }
+                Console.Write("Please enter a last name: ");
+                string last = Console.ReadLine();
+                Console.Write("Please enter an ID: ");
+                int userId = Int32.Parse(Console.ReadLine());
+                Console.Write("Please enter a photo url: ");
+                string url = Console.ReadLine();
+
+                Employee currentEmployee = new(first, last, userId, url);
+                employees.Add(currentEmployee);
             }
 
-            // for (int i = 0; i < employees.Count; i++)
-            // {
-            //     Console.WriteLine(employees[i]);
-            // }
+            return employees;
+        }
+
+        static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = GetEmployees();
+            PrintEmployees(employees);
         }
     }
 }
