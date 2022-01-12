e93b83a8b3c28a5af46c87b0657680c901a648e8
Daniel Zamanillo <dzamanillok@gmail.com>
2022-01-12 11:25:09 -0600

    first push

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// // V6.0
+// Console.WriteLine("Hello, World!");
+
+
+//V5.?
+using System.Collections.Generic;
+
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>(){
+                "Adam",
+                "Alex",
+                "Bryan",
+                "Dan",
+                "Dom",
+                "JP",
+                "Wils"
+            };
+
+            employees.Add("Brooke");
+            employees.Add("Link");
+
+            foreach (string employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+            // for (int i = 0; i < employees.Count; i++)
+            // {
+            //     Console.WriteLine(employees[i]);
+            // }
+        }
+    }
+}
+
